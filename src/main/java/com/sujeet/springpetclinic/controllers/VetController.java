061b9bf0f22fe3e4a27e295a614bb88955db48af
Sujeet3099 <sujeetk3099@gmail.com>
2021-12-28 17:16:56 +0530

    Listed All Vets on Vets Index Page | Closes #14

--- src/main/java/com/sujeet/springpetclinic/controllers/VetController.java
+++ src/main/java/com/sujeet/springpetclinic/controllers/VetController.java
@@ -1,14 +1,22 @@
 package com.sujeet.springpetclinic.controllers;
 
+import com.sujeet.springpetclinic.service.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/vets")
 @Controller
 public class VetController {
+    private final VetService vetService;
 
-    @RequestMapping({"","/","index","index.html"})
-    public String getVetsList(){
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
+    @RequestMapping({"", "/", "index", "index.html"})
+    public String getVetsList(Model model) {
+        model.addAttribute("vets",vetService.findAll());
         return "vets/index";
     }
 }
